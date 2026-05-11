7cf016ae8b88d0632c32d38694e1e160d858a7b9
Munish Jalota <Munish.Jalota@justice.gov.uk>
2026-05-11 20:36:33 +0100

    springboot

--- src/main/java/uk/gov/moj/cp/util/Utils.java
+++ src/main/java/uk/gov/moj/cp/util/Utils.java
@@ -1,8 +1,5 @@
 package uk.gov.moj.cp.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.experimental.UtilityClass;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
--- src/test/java/uk/gov/moj/cp/util/UtilsTest.java
+++ src/test/java/uk/gov/moj/cp/util/UtilsTest.java
@@ -1,12 +1,10 @@
 package uk.gov.moj.cp.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
