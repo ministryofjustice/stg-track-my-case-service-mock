77567f5f999f9e2026ffb0a7e77e5c12c43b1c5b
Vipin Palengara <Vipin.Palengara@justice.gov.uk>
2026-05-12 11:55:21 +0100

    json to object change

--- src/test/java/uk/gov/moj/cp/controller/ProsecutionCaseDetailControllerTest.java
+++ src/test/java/uk/gov/moj/cp/controller/ProsecutionCaseDetailControllerTest.java
@@ -1,32 +1,41 @@
 package uk.gov.moj.cp.controller;
 
+import com.moj.generated.hmcts.ProsecutionCase;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.gov.moj.cp.service.ProsecutionCaseDetailService;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ProsecutionCaseDetailControllerTest {
 
+   /* @Mock
+    ProsecutionCaseDetailService prosecutionCaseDetailService;*/
+
     @Test
     void pcdCaseReturnsServiceBody() throws Exception {
-        ProsecutionCaseDetailService prosecutionCaseDetailService = new ProsecutionCaseDetailService() {
-            @Override
-            public ResponseEntity<String> caseDetail() {
-                return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{\"caseStatus\":\"ACTIVE\"}");
-            }
-        };
+        final String caseurn = "2NXABC12";
+        final ProsecutionCase prosecutionCase = new ProsecutionCase();
+        prosecutionCase.setCaseStatus("ACTIVE");
+        prosecutionCase.setReportingRestrictions(false);
+
+        ProsecutionCaseDetailService prosecutionCaseDetailService = mock(ProsecutionCaseDetailService.class);
+        when(prosecutionCaseDetailService.caseDetail(caseurn)).thenReturn(prosecutionCase);
+
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(
                 new ProsecutionCaseDetailController(prosecutionCaseDetailService)
         ).build();
 
         mockMvc.perform(get("/pcd/cases/any-urn"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"caseStatus\":\"ACTIVE\"}"));
+                .andExpect(jsonPath("$.caseStatus").value("ACTIVE"))
+                .andExpect(jsonPath("$.reportingRestrictions").value(false));
     }
 }
--- src/test/java/uk/gov/moj/cp/service/ProsecutionCaseDetailServiceTest.java
+++ src/test/java/uk/gov/moj/cp/service/ProsecutionCaseDetailServiceTest.java
@@ -1,10 +1,10 @@
 package uk.gov.moj.cp.service;
 
+import com.moj.generated.hmcts.ProsecutionCase;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ProsecutionCaseDetailServiceTest {
 
@@ -12,10 +12,9 @@ class ProsecutionCaseDetailServiceTest {
 
     @Test
     void caseDetailReturnsPcdStubJson() {
-        ResponseEntity<String> response = prosecutionCaseDetailService.caseDetail();
-
-        assertEquals(200, response.getStatusCode().value());
-        assertTrue(response.getBody().contains("ACTIVE"));
-        assertTrue(response.getBody().contains("caseStatus"));
+        String caseUrn = "2NX12W";
+        ProsecutionCase prosecutionCase = prosecutionCaseDetailService.caseDetail(caseUrn);
+        assertEquals("ACTIVE",  prosecutionCase.getCaseStatus());
+        assertFalse(prosecutionCase.isReportingRestrictions());
     }
 }
