d08e2a54cc58c3ea2acbddeadc46ffcdb0c10bce
Vipin Palengara <61233682+meetvipinp@users.noreply.github.com>
2026-05-29 10:41:21 +0100

    update docker build (#32)
    * update docker build

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,26 @@ SERVICE_NAME="stg-track-my-case-service-mock"
 # Check if the Docker container is running
 if [ "$(docker ps -q -f name=${SERVICE_NAME})" ]; then
     echo "Stopping the running Docker container..."
-    docker-compose down
+    docker compose down
 fi
 
 echo "Building application JAR for Docker..."
-./gradlew prepareDocker -Dorg.gradle.daemon=false
+./gradlew bootJar -Dorg.gradle.daemon=false || {
+    echo "ERROR: Gradle build failed."
+    exit 1
+}
 
-if [ ! -f docker/stg-track-my-case-service-mock.jar ]; then
-    echo "ERROR: docker/stg-track-my-case-service-mock.jar was not created. Run: ./gradlew prepareDocker"
+if [ ! -f build/libs/stg-track-my-case-service-mock.jar ]; then
+    echo "ERROR: build/libs/stg-track-my-case-service.jar was not created. Run: ./gradlew bootJar"
     exit 1
 fi
 
 # Rebuild the Docker image
 echo "Rebuilding the Docker image..."
-docker-compose build
+docker compose build
 
 # Deploy the service
 echo "Starting the Docker container..."
-docker-compose up -d
+docker compose up -d
 
 echo "Deployment completed successfully."
